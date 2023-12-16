cf8817e45d2e46bd568395b0cbd898a8d394039e
Shinya Kodaip <k9052726@kadai.jp>
2023-12-16 17:24:52 +0900

    rebuild

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -4,10 +4,10 @@
                <header class="flex justify-between items-center py-4 md:py-8">
                     <router-link to="/" class="inline-flex items-center text-black-800 text-xl md:text-3xl font-bold gap-2.5">Taro Text Portfolio</router-link>
                     <nav class="hidden lg:flex gap-12">
-                         <router-link to="/" class="menu-str">Home</router-link>
+                         <!-- <router-link to="/" class="menu-str">Home</router-link>
                          <router-link to="/#skill" class="menu-str">Skill</router-link>
                          <router-link to="/#experience" class="menu-str">Experience</router-link>
-                         <router-link to="/#works" class="menu-str">Works</router-link>
+                         <router-link to="/#works" class="menu-str">Works</router-link> -->
                     </nav>
                     <button @click="toggleNav" id="menuBtn" type="button" class="inline-flex items-center lg:hidden bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold rounded-lg gap-2 px-2.5 py-2">
                          <svg class="w-6 h-6" id="menuSvg" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
